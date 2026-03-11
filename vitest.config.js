336db09dbcba9e06ce88ed976fb5d34873dea913
Aditya Patadia <aditya@gumlet.com>
2026-03-12 00:13:03 +0530

    coverage report excludes everything in test dir

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: [
+        "test/**/*"
+      ],
       provider: 'v8', // or 'istanbul'
       enabled: true,
       reporter: ['text', 'lcov'], // 'lcov' is crucial for Coveralls
